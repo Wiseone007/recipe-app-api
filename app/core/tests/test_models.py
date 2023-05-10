3afa172ad74bbd0827ebb34c71d0766127239c0f
Yusuf <yusufdoba@gmail.com>
2023-05-10 15:00:38 +0100

    added additional test

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,6 +8,12 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+def create_user(email='user@example.com', password='testpass123'):
+    """Create a return a new user."""
+    return get_user_model().objects.create_user(email, password)
+
+
+
 class ModelTests(TestCase):
     """Test models."""
 
@@ -64,3 +70,4 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(recipe), recipe.title)
+
